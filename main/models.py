4581d5fe12ca2f2094f57874a29dff17a572f09e
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-05-26 14:09:48 +0300

    add company and user profile models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=300)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True)
+    role = models.CharField(max_length=300, blank=True, null=True)
+    slack = models.TextField(max_length=300, blank=True, null=True)
+    interests = models.TextField(max_length=600, blank=True, null=True)
+    location = models.CharField(max_length=300, blank=True, null=True)
+    work_start = models.TimeField(blank=True, null=True)
+    work_end = models.TimeField(blank=True, null=True)
+    agenda = models.TextField(max_length=600, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
