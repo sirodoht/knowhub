7aba08e89636ffbacda1037d901a9534661f5a6d
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-05-26 19:20:46 +0300

    make debug false

--- knowhub/settings.py
+++ knowhub/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ.get('NODEBUG') is None else False
+# DEBUG = True if os.environ.get('NODEBUG') is None else False
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
