1a51a30a3cc57dfc258192949db15ca9b368ed58
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-05-26 10:07:53 +0300

    add auth routes

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,15 @@ admin.site.site_header = 'KnowHub administration'
 app_name = 'main'
 
 urlpatterns = [
+    # /
     path('', views.index, name='index'),
+
+    # /login/
+    path('login/', views.login, name='login'),
+
+    # /auth/
+    path('auth/', views.token_post, name='auth'),
+
+    # /logout/
+    path('logout/', views.logout, name='logout'),
 ]
