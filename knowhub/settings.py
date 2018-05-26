25e25ef673c4505d744a3ef93e55c9ad1ddb0a38
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-05-26 10:04:31 +0300

    add email smtp settings

--- knowhub/settings.py
+++ knowhub/settings.py
@@ -140,6 +140,20 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+
+# Email
+# https://docs.djangoproject.com/en/2.0/topics/email/
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'email-smtp.eu-west-1.amazonaws.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+
+DEFAULT_FROM_EMAIL = 'hi@knowhub.app'
+
+
 # Authentication backends
 # https://docs.djangoproject.com/en/2.0/topics/auth/customizing/
 
