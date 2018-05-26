d88d70a195b5af6df139d3b073acf300aaee4008
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-05-26 20:27:27 +0300

    revert to default settings for staticfiles storage

--- knowhub/settings.py
+++ knowhub/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
