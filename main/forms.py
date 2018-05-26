6ec5428ea0bd3c2d199c6e637c774260c8dc1417
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-05-26 14:09:51 +0300

    add company form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from .models import Company
 
 class EmailForm(forms.Form):
     email = forms.EmailField(label='Your email address')
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ['name']
