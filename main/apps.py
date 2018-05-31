fc03d3ea994dc3627a7b7a4a5bc995cea12d35de
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-05-31 20:54:01 +0300

    add stripe auth on main app ready

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,11 @@
+import stripe
 from django.apps import AppConfig
+
+from knowhub import settings
 
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    def ready(self):
+        stripe.api_key = settings.STRIPE_SECRET
