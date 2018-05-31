9c34df912e8e0530db7894b746a420242e704d32
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-05-31 20:54:29 +0300

    add admin and stripe id fields on profile

--- main/models.py
+++ main/models.py
@@ -11,6 +11,8 @@ class Company(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True)
+    is_admin = models.BooleanField(default=False)
+    stripe_id = models.CharField(max_length=50, blank=True, null=True)
     role = models.CharField(max_length=300, blank=True, null=True)
     slack = models.TextField(max_length=300, blank=True, null=True)
     interests = models.TextField(max_length=600, blank=True, null=True)
