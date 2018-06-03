c0342240fb933fea08f6b77f8251531ea9bf342c
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-06-03 21:21:37 +0300

    add models on admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from . import models
+
+
+# User
+class KnowhubAdmin(UserAdmin):
+    list_display = ('username', 'email', 'date_joined', 'last_login', 'id',)
+
+
+admin.site.unregister(User)
+admin.site.register(User, KnowhubAdmin)
+
+
+# Profile
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company', 'stripe_id', 'id',)
+
+
+admin.site.register(models.Profile, ProfileAdmin)
+
+
+# Company
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id',)
+
+
+admin.site.register(models.Company, CompanyAdmin)
