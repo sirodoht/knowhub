84a1b0901e5a60a8b9df5c698d8a17a9f2376f8e
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-07-27 02:05:49 +0300

    add icons on top level nav

--- main/templatetags/app_filters.py
+++ main/templatetags/app_filters.py
@@ -11,3 +11,11 @@ def nav_active(context, pattern):
     if re.search(pattern, path):
         return "nav-item-active"
     return ""
+
+
+@register.simple_tag(takes_context=True)
+def icon_active(context, pattern):
+    path = context["request"].path
+    if re.search(pattern, path):
+        return "nav-item-icon-active"
+    return ""
