a2d6fc8d8717c98f170eb58804a09ae849df2b61
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-07-26 20:00:11 +0300

    tweaks on scaffold template

---
+++ main/templatetags/app_filters.py
@@ -0,0 +1,12 @@
+import re
+
+from django import template
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def nav_active(context, pattern):
+    path = context['request'].path
+    if re.search(pattern, path):
+        return 'nav-item-active'
+    return ''
