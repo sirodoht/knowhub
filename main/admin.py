e3ddc855f95da5423e09c8d5b9d81b673a9f7123
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-08-01 02:09:22 +0300

    black admin

--- main/admin.py
+++ main/admin.py
@@ -56,6 +56,7 @@ admin.site.register(models.Subscriber, SubscriberAdmin)
 
 # Analytic
 class AnalyticAdmin(admin.ModelAdmin):
-    list_display = ('ip', 'user', 'created_at', 'path', 'querystring')
+    list_display = ("ip", "user", "created_at", "path", "querystring")
+
 
 admin.site.register(models.Analytic, AnalyticAdmin)
