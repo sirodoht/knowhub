55df2bb10bf395bd14b8822699915aedce99f2c8
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-08-01 01:59:13 +0300

    add get client ip helper

--- main/helpers.py
+++ main/helpers.py
@@ -91,3 +91,12 @@ def verify_invite_data(token=None):
         user.save()
 
     return user
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
