8b37f19c23c50c267a365db328e74592e8fafcca
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-08-21 22:18:00 +0300

    change login redirect in settings

--- knowhub/settings.py
+++ knowhub/settings.py
@@ -143,7 +143,7 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
 )
 
-LOGIN_URL = "/signin"
+LOGIN_URL = "main:login"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
