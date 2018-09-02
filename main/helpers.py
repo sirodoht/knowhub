cf518cce8c9adcc0bc24d1be0f353ca18d019b2f
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-09-03 01:18:18 +0300

    add open invite helper functions

--- main/helpers.py
+++ main/helpers.py
@@ -12,6 +12,7 @@ from django.core.mail import send_mail
 from django.core.signing import BadSignature, Signer
 from django.template.loader import render_to_string
 from django.utils.text import slugify
+from django.urls import reverse
 
 from knowhub import settings
 
@@ -60,7 +61,7 @@ def get_invite_data(request, email, company):
     signed_info = Signer().sign(base64.b64encode(info).decode("utf8"))
 
     domain = get_current_site(request).domain
-    invitation_url = domain + "/invitation/?d=" + signed_info
+    invitation_url = domain + reverse('main:invite_verify') + "?d=" + signed_info
     if "localhost" in domain or "127.0.0.1" in domain:
         invitation_url = "http://" + invitation_url
     else:
@@ -140,3 +141,53 @@ def get_timezones_form():
     timezones_synthesized = [*timezones_minus, *timezones_utc, *timezones_plus]
 
     return timezones_synthesized
+
+
+def get_open_invite(request, refresh=False):
+    company = request.user.profile.company
+
+    if not company.invite_data or refresh:
+        data = {
+            "t": int(time.time()),
+            "c": company.route,
+        }
+
+        data = json.dumps(data).encode("utf8")
+        data = Signer().sign(base64.b64encode(data).decode("utf8"))
+
+        company.invite_data = data
+        company.save()
+
+    domain = get_current_site(request).domain
+    open_invite_url = domain + reverse('main:invite_open_verify') + "?d=" + company.invite_data
+    if "localhost" in domain or "127.0.0.1" in domain:
+        open_invite_url = "http://" + open_invite_url
+    else:
+        open_invite_url = "https://" + open_invite_url
+
+    return open_invite_url
+
+
+def verify_open_invite_data(token=None):
+    try:
+        data = Signer().unsign(token)
+    except BadSignature:
+        return
+
+    data = json.loads(base64.b64decode(data).decode("utf8"))
+
+    # check if there is company info in invite data
+    if not data["c"]:
+        return
+
+    # check if company info is vaid
+    company_route = data["c"]
+    if not Company.objects.filter(route=company_route).exists():
+        return
+
+    # check if company info hasn't been reset
+    company = Company.objects.get(route=company_route)
+    if company.invite_data != token:
+        return
+
+    return company_route
