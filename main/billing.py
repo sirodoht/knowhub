ca8ab312f7bb5c42a6e04f45f5ca031b7000ff04
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-09-04 03:16:05 +0200

    add info get and card change functions on billing module

--- main/billing.py
+++ main/billing.py
@@ -34,3 +34,26 @@ def subscription_upgrade(user, added_quantity=1):
     new_quantity = quantity + added_quantity
     subscription.quantity = new_quantity
     return subscription.save()
+
+
+def info_get(stripe_id):
+    customer = stripe.Customer.retrieve(stripe_id)
+    info = {}
+    info["brand"] = customer["sources"]["data"][0]["brand"]
+    info["exp_month"] = customer["sources"]["data"][0]["exp_month"]
+    info["exp_year"] = customer["sources"]["data"][0]["exp_year"]
+    info["last4"] = customer["sources"]["data"][0]["last4"]
+    info["quantity"] = customer["subscriptions"]["data"][0]["quantity"]
+    info["cost"] = 2 * info["quantity"]
+    info["trial"] = False
+    if customer["subscriptions"]["data"][0]["status"] == "trialing":
+        info["trial"] = True
+    return info
+
+
+def card_change(stripe_id, new_token):
+    if stripe_id is None:
+        return
+    customer = stripe.Customer.retrieve(stripe_id)
+    customer.source = new_token
+    return customer.save()
